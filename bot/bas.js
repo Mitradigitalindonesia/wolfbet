27c3b2ba4cb8f0e0b51ff481584ed7bfb3fea085
Mitradigitalindonesia <mitradigitalindonesia99@gmail.com>
2025-05-27 14:00:41 +0700

    Update bas.js

--- bot/bas.js
+++ bot/bas.js
@@ -1,5 +1,11 @@
 module.exports = class BaseDice {
-  constructor(proxy) {
-    this.proxy = proxy || {};
+  constructor(proxy = {}) {
+    this.proxy = proxy;
+    this.url = ''; // bisa diatur ulang oleh child class seperti wolfbet.js
+    this.benefit = '';
+  }
+
+  log(message) {
+    console.log(`[BaseDice] ${message}`);
   }
 };
