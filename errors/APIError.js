931e49b544e895fd7c48ee8f0ddc962c799de1fd
Mitradigitalindonesia <mitradigitalindonesia99@gmail.com>
2025-05-27 13:07:30 +0700

    Update APIError.js

--- errors/APIError.js
+++ errors/APIError.js
@@ -1,1 +1,13 @@
-r
+module.exports  = class APIError extends Error {
+  constructor(message, obj) {
+    super(message);
+    this.name = 'APIError';
+    this.error = obj.value;
+    this.description = obj.value;
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error(message).stack;
+    }
+  }
+}
